c608aa09c356b8d8aeaf251d7042ec755cbe321c
Blago Gunev <bgunev@gmail.com>
2024-09-27 20:48:01 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nixos config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs: 
+  let 
+    outputs = inputs.self.outputs;
+
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in 
+  {
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+         
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
