6cd3ad938b8901f797a7643cc7e690e159acc564
Blago Gunev <bgunev@gmail.com>
2024-09-30 12:30:49 +0200

    Bump to newer kernel

--- configuration.nix
+++ configuration.nix
@@ -15,6 +15,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda";
   boot.loader.grub.useOSProber = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
