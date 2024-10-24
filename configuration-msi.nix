0cdf08d520c5da5dddc15edb84f2e41618d9784d
Blago Gunev <bgunev@gmail.com>
2024-10-24 10:11:05 +0200

    Add sunshine to msi config

--- configuration-msi.nix
+++ configuration-msi.nix
@@ -14,9 +14,10 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "msi-host"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -76,6 +77,13 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+  };
+
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true;
+    openFirewall = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
