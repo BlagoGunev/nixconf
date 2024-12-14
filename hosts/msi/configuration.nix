f6658c4adc7676f257f9f6d692578d47d027409c
Blago Gunev <bgunev@gmail.com>
2024-12-14 23:12:35 +0200

    Revert Triple buffering patch

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -85,20 +85,20 @@
   services.xserver.desktopManager.gnome.enable = true;
 
   # Gnome performance improvements
-  nixpkgs.overlays = [
-    # GNOME 46: triple-buffering-v4-46
-    (final: prev: {
-      mutter = prev.mutter.overrideAttrs (old: {
-        src = pkgs.fetchFromGitLab  {
-          domain = "gitlab.gnome.org";
-          owner = "vanvugt";
-          repo = "mutter";
-          rev = "triple-buffering-v4-46";
-          hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
-        };
-      });
-    })
-  ];
+  # nixpkgs.overlays = [
+  #   # GNOME 46: triple-buffering-v4-46
+  #   (final: prev: {
+  #     mutter = prev.mutter.overrideAttrs (old: {
+  #       src = pkgs.fetchFromGitLab  {
+  #         domain = "gitlab.gnome.org";
+  #         owner = "vanvugt";
+  #         repo = "mutter";
+  #         rev = "triple-buffering-v4-46";
+  #         hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
+  #       };
+  #     });
+  #   })
+  # ];
 
   # Configure keymap in X11
   services.xserver.xkb = {
