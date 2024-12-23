47c3894807e0037daaa602b481a02460d4247223
Blago Gunev <bgunev@gmail.com>
2024-12-24 00:19:27 +0200

    Patch kernel for WoL

--- hosts/msi/configuration.nix
+++ hosts/msi/configuration.nix
@@ -46,6 +46,16 @@
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
     kernelPackages = pkgs.linuxPackages_latest;
+
+    kernelPatches = [
+      {
+        name = "atheros e2500 wol";
+        patch = builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/jhwshin/alx-wol-dkms/refs/heads/main/alx-wol.patch";
+          sha256 = "1rxjg80ab1v59bfa98rn9qz1n096cj9vscmn63hzdsw1ihh0vwgl";
+        };
+      }
+    ];
   };
 
 
