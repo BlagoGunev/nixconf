11319ff01259505890d2c4821508f395d8caa6bb
Blago Gunev <38541383+BlagoGunev@users.noreply.github.com>
2025-08-08 12:02:43 +0300

    MSI version bump

--- hosts/msi/hardware-configuration.nix
+++ hosts/msi/hardware-configuration.nix
@@ -8,13 +8,13 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" "rtsx_usb_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "sd_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/53f5b6b7-d3b6-4139-841d-11550989b339";
+    { device = "/dev/disk/by-uuid/aa3edd12-ae07-4e4a-8a14-4f5127ff9c8c";
       fsType = "ext4";
     };
 
